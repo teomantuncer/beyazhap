da2c03edd752e16800b115824f2c39915bbffb2a
Teoman Tuncer <teomantuncer@gmail.com>
2021-01-09 20:36:49 +0200

    slider kaldırıldı, package.json düzenlendi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Splide, SplideSlide } from '@splidejs/react-splide'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
@@ -8,24 +7,13 @@ const IndexPage = ({
 	location,
 	data
 }) => {
-	if (typeof window === "undefined") {
-		return <p>Server Render</p>
-	}
-	const rand = Math.floor(Math.random() * data.allFile.edges.length - 3)
+	const rand = Math.floor(Math.random() * data.allFile.edges.length - 1)
 	return <Layout location={location}>
 		<SEO title="Ana Sayfa" />
-		<Splide options={{
-			rewind: true,
-			autoplay: true,
-			arrows: false
-		}}>
-			{data.allFile.edges.slice(rand, rand + 3)
-				.map(edge => {
-					return <SplideSlide key={edge.node.id}>
-						<Image filename={edge.node.base} />
-					</SplideSlide>
-				})}
-		</Splide>
+		{data.allFile.edges.slice(rand, rand + 1)
+			.map(edge => {
+				return <Image filename={edge.node.base} key={edge.node.id} />
+			})}
 	</Layout>
 }
 export const query = (
